7417cec9cba51d8543292b6eddc86fb0bc701694
Antonio <41914149+Craftinoh@users.noreply.github.com>
2026-08-06 14:56:06 +0200

    Fix WorldEdit 6 and legacy FAWE selection bridge

--- legacy-regions/src/main/java/it/legacynetwork/regions/selection/WorldEditSelectionProvider.java
+++ legacy-regions/src/main/java/it/legacynetwork/regions/selection/WorldEditSelectionProvider.java
@@ -1,100 +1,79 @@
 package it.legacynetwork.regions.selection;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public final class WorldEditSelectionProvider implements SelectionProvider {
 
-    private final boolean available;
-    private final Object worldEditPlugin;
-    private final Method getWorldEditMethod;
-    private final Method getSessionMethod;
-    private final Method getRegionSelectorMethod;
-    private final Method getRegionMethod;
-    private final Method getMinimumPointMethod;
-    private final Method getMaximumPointMethod;
-    private final Method getBlockXMethod;
-    private final Method getBlockYMethod;
-    private final Method getBlockZMethod;
+    private final Plugin worldEditPlugin;
+    private final Method getSelectionMethod;
 
-    public WorldEditSelectionProvider(Object worldEditPluginInstance) {
-        this.worldEditPlugin = worldEditPluginInstance;
-        boolean avail = false;
-        Method getWE = null;
-        Method getSession = null;
-        Method getRegionSelector = null;
-        Method getRegion = null;
-        Method getMinPoint = null;
-        Method getMaxPoint = null;
-        Method getBlockX = null;
-        Method getBlockY = null;
-        Method getBlockZ = null;
-
-        try {
-            Class<?> wePluginClass = Class.forName("com.sk89q.worldedit.bukkit.WorldEditPlugin");
-            Class<?> localSessionClass = Class.forName("com.sk89q.worldedit.LocalSession");
-            Class<?> worldEditClass = Class.forName("com.sk89q.worldedit.WorldEdit");
-            Class<?> actorClass = Class.forName("com.sk89q.worldedit.extension.platform.Actor");
-            Class<?> regionSelectorClass = Class.forName("com.sk89q.worldedit.regions.selector.RegionSelector");
-            Class<?> regionClass = Class.forName("com.sk89q.worldedit.regions.Region");
-            Class<?> vectorClass = Class.forName("com.sk89q.worldedit.Vector");
-            Class<?> worldClass = Class.forName("com.sk89q.worldedit.LocalWorld");
-
-            getWE = wePluginClass.getMethod("getWorldEdit");
-            getSession = worldEditClass.getMethod("getSession", actorClass);
-            getRegionSelector = localSessionClass.getMethod("getRegionSelector", worldClass);
-            getRegion = regionSelectorClass.getMethod("getRegion");
-            getMinPoint = regionClass.getMethod("getMinimumPoint");
-            getMaxPoint = regionClass.getMethod("getMaximumPoint");
-            getBlockX = vectorClass.getMethod("getBlockX");
-            getBlockY = vectorClass.getMethod("getBlockY");
-            getBlockZ = vectorClass.getMethod("getBlockZ");
-
-            avail = true;
-        } catch (Exception e) {
-            avail = false;
+    public WorldEditSelectionProvider(Plugin worldEditPlugin) {
+        if (worldEditPlugin == null) {
+            throw new IllegalArgumentException("Plugin WorldEdit mancante");
         }
-
-        this.available = avail;
-        this.getWorldEditMethod = getWE;
-        this.getSessionMethod = getSession;
-        this.getRegionSelectorMethod = getRegionSelector;
-        this.getRegionMethod = getRegion;
-        this.getMinimumPointMethod = getMinPoint;
-        this.getMaximumPointMethod = getMaxPoint;
-        this.getBlockXMethod = getBlockX;
-        this.getBlockYMethod = getBlockY;
-        this.getBlockZMethod = getBlockZ;
+        this.worldEditPlugin = worldEditPlugin;
+        try {
+            this.getSelectionMethod = worldEditPlugin.getClass()
+                    .getMethod("getSelection", Player.class);
+        } catch (NoSuchMethodException exception) {
+            throw new IllegalStateException(
+                    "API legacy getSelection(Player) non disponibile", exception);
+        }
     }
 
     @Override
     public boolean isAvailable() {
-        return available;
+        return worldEditPlugin.isEnabled() && getSelectionMethod != null;
     }
 
     @Override
-    public int[] getSelection(Player player) {
-        if (!available) {
+    public RegionSelection getSelection(Player player) {
+        if (!isAvailable() || player == null) {
             return null;
         }
         try {
-            Object worldEdit = getWorldEditMethod.invoke(worldEditPlugin);
-            Object localSession = getSessionMethod.invoke(worldEdit, player);
-            Object bukkitWorld = player.getWorld();
-            Object regionSelector = getRegionSelectorMethod.invoke(localSession, bukkitWorld);
-            Object region = getRegionMethod.invoke(regionSelector);
-            Object minPoint = getMinimumPointMethod.invoke(region);
-            Object maxPoint = getMaximumPointMethod.invoke(region);
-            int minX = (Integer) getBlockXMethod.invoke(minPoint);
-            int minY = (Integer) getBlockYMethod.invoke(minPoint);
-            int minZ = (Integer) getBlockZMethod.invoke(minPoint);
-            int maxX = (Integer) getBlockXMethod.invoke(maxPoint);
-            int maxY = (Integer) getBlockYMethod.invoke(maxPoint);
-            int maxZ = (Integer) getBlockZMethod.invoke(maxPoint);
+            Object selection = getSelectionMethod.invoke(worldEditPlugin, player);
+            if (selection == null) {
+                return null;
+            }
 
-            return new int[]{minX, minY, minZ, maxX, maxY, maxZ};
-        } catch (Exception e) {
+            Method getMinimumPoint = selection.getClass()
+                    .getMethod("getMinimumPoint");
+            Method getMaximumPoint = selection.getClass()
+                    .getMethod("getMaximumPoint");
+            Object minimum = getMinimumPoint.invoke(selection);
+            Object maximum = getMaximumPoint.invoke(selection);
+            if (!(minimum instanceof Location) || !(maximum instanceof Location)) {
+                return null;
+            }
+
+            Location min = (Location) minimum;
+            Location max = (Location) maximum;
+            World world = min.getWorld() != null ? min.getWorld() : player.getWorld();
+            if (world == null) {
+                return null;
+            }
+            if (max.getWorld() != null
+                    && !max.getWorld().getUID().equals(world.getUID())) {
+                return null;
+            }
+
+            return new RegionSelection(
+                    world.getName(),
+                    world.getUID().toString(),
+                    min.getBlockX(), min.getBlockY(), min.getBlockZ(),
+                    max.getBlockX(), max.getBlockY(), max.getBlockZ());
+        } catch (InvocationTargetException exception) {
+            return null;
+        } catch (ReflectiveOperationException exception) {
+            return null;
+        } catch (LinkageError exception) {
             return null;
         }
     }
