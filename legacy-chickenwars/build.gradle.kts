28b33727d4304a1cb0d59269ec508d5a8885419b
Antonio <41914149+Craftinoh@users.noreply.github.com>
2026-08-07 13:20:52 +0200

    fix(chickenwars): do not shade language common

--- legacy-chickenwars/build.gradle.kts
+++ legacy-chickenwars/build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":language-common"))
+    compileOnly(project(":language-common"))
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 
+    testImplementation(project(":language-common"))
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
